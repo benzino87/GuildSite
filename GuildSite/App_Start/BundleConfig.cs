4d50f72d234828a8e19c3db323a1f3ce4a00a60c
benselj <benselj@MIL-BENSELJ10>
2016-08-21 00:09:02 -0400

    Updated home page, added some CSS to give look and feel

--- GuildSite/App_Start/BundleConfig.cs
+++ GuildSite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace GuildSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/index.css"));
         }
     }
 }
